72f89985dcaf7171599248cb8e7c4ed1a9685dbb
Alexis Guevara <alexis.guevara@itksquare.edu.mx>
2023-03-23 21:41:43 -0700

    UserStory5: choose 12 Pokémon, add styles, comment code

--- script.js
+++ script.js
@@ -1,56 +1,54 @@
 
+// Get main container from HTML
 const pokeList = document.getElementById('pokeList');
 
+// Pokemon API
+let url = 'https://pokeapi.co/api/v2/pokemon/';
 
-let showPokemon = (i) => {
+// Request 12 pokemons from the Pokemon API
+for (let i = 1; i <= 12; i++) {    
+    fetch(url + i)
+    .then((response) => response.json())
+    .then(data => showPokemon(data));
+}
+
+let showPokemon = (pokeData) => {
+
+    // Get the pokemon type thru a map method so we can extract all we need and convert into a "p" tag
+    let poketypes = pokeData.types.map(type => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
+    // Join the array
+    poketypes = poketypes.join('');
+
+    // Get the pokemon stats thru a map method so we can extract all we need and convert into a "p" tag
+    let pokeStats = pokeData.stats.map(stats => `<p class="stat">${stats.stat.name}: ${stats.base_stat}</p>`);
+    // Slice the info that we don't need
+    pokeStats.splice(3,2);
+    // Join the array
+    pokeStats = pokeStats.join('');
+
+    // Create div for each pokemon we receive from the API
     const div = document.createElement('div');
+    // Assign name to class
     div.classList.add('pokemon');
+    // Send the HTML template
     div.innerHTML = `
-    <div class="pokemon-image">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon">
+    <div class="pokemonImg">
+        <img src="${pokeData.sprites.other['official-artwork'].front_default}" alt="${pokeData.name}">
     </div>
-    <div class="pokemon-info">
-        <div class="nombre-contenedor">
-            <h2 class="pokemon-nombre">Bulbasaur</h2>
+    <div class="pokemonInfo">
+        <div class="-nameContainer">
+            <h2 class="pokemonName">${pokeData.name}</h2>
         </div>
-        <div class="pokemon-tipos">
-            <p class="grass tipo">Grass</p>
-            <p class="poison tipo">Poison</p>
+        <div class="pokemonTypes">
+            ${poketypes}
         </div>
-        <div class="pokemon-stats">
-            <p class="stat">attack</p>
-            <p class="stat">hp</p>
-            <p class="stat">defense</p>
-            <p class="stat">speed</p>
+        <div class="pokemonStats">
+            ${pokeStats}
         </div>
     </div>
     `;
     pokeList.append(div);
 };
 
-for (let i = 1; i <= 12; i++) {    
-    showPokemon(i);
-}
-
-/*
-<div class="pokemon">
-    <div class="pokemon-image">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon">
-    </div>
-    <div class="pokemon-info">
-        <div class="nombre-contenedor">
-            <h2 class="pokemon-nombre">Bulbasaur</h2>
-        </div>
-        <div class="pokemon-tipos">
-            <p class="grass tipo">Grass</p>
-            <p class="poison tipo">Poison</p>
-        </div>
-        <div class="pokemon-stats">
-            <p class="stat">attack</p>
-            <p class="stat">hp</p>
-            <p class="stat">defense</p>
-            <p class="stat">speed</p>
-        </div>
-    </div>
-</div>
-*/
+// Execute function so it can load Pokemon
+showPokemon();
