b457a30040c7ed9944a7f8648311fdebd548d057
Alexis Guevara <alexis.guevara@itksquare.edu.mx>
2023-03-23 19:15:09 -0700

    UserStory4: card information

--- script.js
+++ script.js
@@ -5,10 +5,52 @@ const pokeList = document.getElementById('pokeList');
 let showPokemon = (i) => {
     const div = document.createElement('div');
     div.classList.add('pokemon');
-    div.innerHTML = `Card: ${i}`;
+    div.innerHTML = `
+    <div class="pokemon-image">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon">
+    </div>
+    <div class="pokemon-info">
+        <div class="nombre-contenedor">
+            <h2 class="pokemon-nombre">Bulbasaur</h2>
+        </div>
+        <div class="pokemon-tipos">
+            <p class="grass tipo">Grass</p>
+            <p class="poison tipo">Poison</p>
+        </div>
+        <div class="pokemon-stats">
+            <p class="stat">attack</p>
+            <p class="stat">hp</p>
+            <p class="stat">defense</p>
+            <p class="stat">speed</p>
+        </div>
+    </div>
+    `;
     pokeList.append(div);
 };
 
 for (let i = 1; i <= 12; i++) {    
     showPokemon(i);
 }
+
+/*
+<div class="pokemon">
+    <div class="pokemon-image">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon">
+    </div>
+    <div class="pokemon-info">
+        <div class="nombre-contenedor">
+            <h2 class="pokemon-nombre">Bulbasaur</h2>
+        </div>
+        <div class="pokemon-tipos">
+            <p class="grass tipo">Grass</p>
+            <p class="poison tipo">Poison</p>
+        </div>
+        <div class="pokemon-stats">
+            <p class="stat">attack</p>
+            <p class="stat">hp</p>
+            <p class="stat">defense</p>
+            <p class="stat">speed</p>
+        </div>
+    </div>
+</div>
+*/
